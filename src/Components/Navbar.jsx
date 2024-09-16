74a2af77244b365dda82456ee73723fa3c2b02f4
PRAJAPATI HARSH <harshprajapati8583@gmail.com>
2024-09-16 23:35:28 +0530

    fix nav

--- src/Components/Navbar.jsx
+++ src/Components/Navbar.jsx
@@ -1,21 +1,39 @@
-import React from "react";
+import React, { useState } from "react";
 import { useAuth } from "../context/contextapi";
 import { NavLink } from "react-router-dom";
 
 const Navbar = () => {
   const { isLogin, logout, userType } = useAuth();
-
+  const [menuOpen, setMenuOpen] = useState(false);
+  const toggleMenu = () => {
+    setMenuOpen(!menuOpen);
+  };
   return (
-    <nav className="bg-white  md:h-[14vh] shadow-md sticky top-0 z-10 w-[100vw] ">
-      <div className="container mx-auto flex items-center justify-between p-4">
+    <nav className="bg-white  md:h-[14vh] shadow-md sticky top-0 z-10 w-[100vw]  flex">
+      <div className="container mx-auto flex items-center justify-between  p-4">
+        <div className="md:hidden mx-2 ">
+          <img
+            className="invert  cursor-pointer mx-1 m-auto flex"
+            src={menuOpen ? "/img/close.svg" : "/img/hamburger.svg"}
+            onClick={toggleMenu}
+            alt="menu"
+          />
+        </div>
         <img
           src="/logo.png" // Path to the image in the public directory
           alt="Logo"
           width={120}
           height={50}
-          className="h-auto"
+          className="h-auto mx-12 "
         />
-        <div className="flex space-x-4">
+        <div
+          className={`humbar md:gap-5 md:justify-center fixed  z-50 top-[15vh] text-xl  md:top-0 right-0 h-full w-2/3  text-white transform ${
+            menuOpen
+              ? "translate-x-0 flex flex-col bg-[#0a131d] p-5 gap-6"
+              : "translate-x-full"
+          } transition-transform duration-300 ease-in-out md:relative md:transform-none md:flex md:items-center`}
+        >
+          
           {/* Example links */}
           <NavLink
             to="/"
@@ -48,23 +66,25 @@ const Navbar = () => {
           >
             Contact
           </NavLink>
+          <div className="md:ml-[250px]">
+            {isLogin ? (
+              <div className="border p-2 md:px-3 rounded bg-slate-400">
+                <NavLink to={"/logout"}>
+                  <button type="button">Logout</button>
+                </NavLink>
+              </div>
+            ) : (
+              <div className="flex gap-2 flex-col md:flex-row">
+                <div className="border p-2 md:px-3 rounded bg-slate-400">
+                  <NavLink to={"/login"}>Login </NavLink>
+                </div>
+                <div className="border p-2 md:px-3 rounded bg-slate-400">
+                  <NavLink to={"/register"}>Sign up</NavLink>
+                </div>
+              </div>
+            )}
+          </div>
         </div>
-        {isLogin ? (
-          <div className="border p-2 px-3 rounded bg-slate-400">
-            <NavLink to={"/logout"}>
-              <button type="button">Logout</button>
-            </NavLink>
-          </div>
-        ) : (
-          <div className="flex gap-2">
-            <div className="border p-2 px-3 rounded bg-slate-400">
-              <NavLink to={"/login"}>Login </NavLink>
-            </div>
-            <div className="border p-2 px-3 rounded bg-slate-400">
-              <NavLink to={"/register"}>Sign up</NavLink>
-            </div>
-          </div>
-        )}
       </div>
     </nav>
   );
